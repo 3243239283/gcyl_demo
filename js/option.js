47f3e1f3a8a5d8d50dce0e8bac126d921c498358
3243239283 <treeclass@qq.com>
2016-12-20 17:26:38 +0800

    买家版修改

--- js/option.js
+++ js/option.js
@@ -1,4 +1,10 @@
-define(['jquery','index'],function(jquery){
-    
+define(['jquery','index','flexslider','cssjs!flexslidercss'],function(jquery,flexslider){
+    $(".flexslider").flexslider({
+    	animation: "slide",
+    	animationLoop: true,
+    	slideshow:true,
+    	slideshowSpeed: 5000,
+    	directionNav: false
+    });
 
 });
